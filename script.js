29ea9710175bea80fc399c1250fa2ab6c119b3cd
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2021-10-25 16:04:40 +0200

    build html frame, edit css and js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function loadEvent() {
-  console.log("Az oldal betöltődött")
+  console.log("Az oldal betöltődött");
+
+  function clickMenuBtn (event) {
+    event.target.classList.toggle("clicked");
+  }
+
+  document.getElementById("menu-btn").addEventListener("click", clickMenuBtn);
 }
 
 window.addEventListener("load", loadEvent);
