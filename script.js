36a40f24cf513042d0018528c1cba34fb5f368c6
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2021-10-26 15:04:43 +0200

    tests on script js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+function fastLog(text){
+console.log(text);
+return "muhaha";
+}
+
 function loadEvent() {
   console.log("Az oldal betöltődött");
 
   function clickMenuBtn (event) {
     event.target.classList.toggle("clicked");
+
+    const fastLogResult = fastLog("Ezt a szöveget szeretném kiloggoltatni");
+    console.log(fastLogResult);
   }
 
   document.getElementById("menu-btn").addEventListener("click", clickMenuBtn);
 }
+fastLog("na ez minden függvényen kívül van");
 
 window.addEventListener("load", loadEvent);
