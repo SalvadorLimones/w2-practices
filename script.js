4a28cf6d3ed303a6edbd0e07f2ac8bb2820d2e05
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2021-10-26 15:42:17 +0200

    add navbar click event

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-function fastLog(text){
-console.log(text);
-return "muhaha";
+function toggleClicked (event) {
+  event.target.classList.toggle("clicked");
+  document.getElementById("nav").classList.toggle("clicked");
 }
 
 function loadEvent() {
   console.log("Az oldal betöltődött");
 
-  function clickMenuBtn (event) {
-    event.target.classList.toggle("clicked");
-
-    const fastLogResult = fastLog("Ezt a szöveget szeretném kiloggoltatni");
-    console.log(fastLogResult);
-  }
-
-  document.getElementById("menu-btn").addEventListener("click", clickMenuBtn);
+  document.getElementById("menu-btn").addEventListener("click", toggleClicked);
 }
-fastLog("na ez minden függvényen kívül van");
 
 window.addEventListener("load", loadEvent);
